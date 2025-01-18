2e7f0775aa60848c2b851405525b54ab2f21a193
iaroslav <varfolomeeviaroslav@gmail.com>
2025-01-18 22:15:47 +0000

    added axis

--- object_3d.py
+++ object_3d.py
@@ -14,6 +14,10 @@ class Object3D:
             self.faces = np.array([np.array(face) for face in faces], dtype="int")
         except:
             self.faces = np.array([np.array(face) for face in faces], dtype="object")
+        self.font = pg.font.SysFont('Arial', 30, bold=True)
+        self.color_faces = [((255, 255, 255), face) for face in self.faces]
+        self.movement_flag, self.draw_vertexes = True, False
+        self.label = ''
 
     def draw(self):
         self.screen_projection()
@@ -26,13 +30,18 @@ class Object3D:
         vertexes = vertexes @ self.render.projection.to_screen_matrix
         vertexes = vertexes[:, :2]
 
-        for face in self.faces:
+        for index, color_face in enumerate(self.color_faces):
+            color, face = color_face
             polygon = vertexes[face]
             if not any_func(polygon, self.render.H_WIDTH, self.render.H_HEIGHT):
-                pg.draw.polygon(self.render.screen, (255, 255, 255), polygon, 1)
-        #for vertex in vertexes:
-        #    if not any_func(vertex, self.render.H_WIDTH, self.render.H_HEIGHT):
-        #        pg.draw.circle(self.render.screen, (255, 0, 0), vertex, 2)
+                pg.draw.polygon(self.render.screen, color, polygon, 1)
+                if self.label:
+                    text = self.font.render(self.label[index], True, (255, 255, 255))
+                    self.render.screen.blit(text, polygon[-1])
+        if self.draw_vertexes:
+            for vertex in vertexes:
+                if not any_func(vertex, self.render.H_WIDTH, self.render.H_HEIGHT):
+                    pg.draw.circle(self.render.screen, (255, 0, 0), vertex, 2)
 
     def translate(self, pos):
         self.vertexes = self.vertexes @ translate(pos)
@@ -48,3 +57,11 @@ class Object3D:
 
     def rotate_z(self, angle):
         self.vertexes = self.vertexes @ rotate_z(angle)
+
+class Axes(Object3D):
+    def __init__(self, render):
+        super().__init__(render, np.array([(0, 0, 0, 1), (1, 0, 0, 1), (0, 1, 0, 1), (0, 0, 1, 1)]), np.array([(0, 1), (0, 2), (0, 3)]))
+        self.colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+        self.color_faces = [(color, face) for color, face in zip(self.colors, self.faces)]
+        self.draw_vertexes = False
+        self.label = 'XYZ'
