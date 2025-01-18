ad53583d3673cf00b87e5b137d496619a3d1ddd5
iaroslav <varfolomeeviaroslav@gmail.com>
2025-01-18 16:24:51 +0000

    add movement

--- main.py
+++ main.py
@@ -20,6 +20,17 @@ class SoftwareRender:
         self.object.translate([0.2, 0.4, 0.2])
         self.object.rotate_y(math.pi / 6)
 
+    def get_objec_from_file(self, filename):
+        vertex, faces = [], []
+        with open(filename) as f:
+            for line in f:
+                if line.startswith('v '):
+                    vertex.append([float(i) for i in line.split()[1:]] + [1])
+                elif line.startswith('f'):
+                    faces_ = line.split()[1:]
+                    faces.append([int(face_.split('/')[0]) - 1 for face_ in faces_])
+        return Object3D(self, vertex, faces)
+
     def draw(self):
         self.screen.fill((0, 0, 0))
         self.object.draw()
@@ -27,6 +38,7 @@ class SoftwareRender:
     def run(self):
         while True:
             self.draw()
+            self.camera.control()
             [exit() for i in pg.event.get() if i.type == pg.QUIT]
             pg.display.set_caption(f'{self.clock.get_fps():.3f}')
             pg.display.flip()
